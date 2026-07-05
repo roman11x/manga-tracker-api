09ecbd56c6a840c94bc1d99c19146c1d578072f1
roman11x <romanmax1000@gmail.com>
2026-07-05 17:00:37 +0300

    Initial Commit, implemented ping test

---
+++ src/main/java/com/roman/mangaapi/MangaTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.roman.mangaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaTrackerApiApplication.class, args);
+	}
+
+}
