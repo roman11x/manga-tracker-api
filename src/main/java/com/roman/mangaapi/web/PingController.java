ad2053a8d9342239ddc63013beaf499443d17d9c
roman11x <romanmax1000@gmail.com>
2026-07-07 15:52:27 +0300

    added count debug method to make sure api connects to the db

--- src/main/java/com/roman/mangaapi/web/PingController.java
+++ src/main/java/com/roman/mangaapi/web/PingController.java
@@ -1,6 +1,8 @@
 package com.roman.mangaapi.web;
 
+import com.roman.mangaapi.db.MangaRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -9,10 +11,19 @@ import java.util.Map;
  * Simple controller to check if the application is running.
  */
 @RestController
+@RequestMapping("/api")
 public class PingController {
+    private final MangaRepository repo;
+    public PingController(MangaRepository repo) {
+        this.repo = repo;
+    }
 
-    @GetMapping("/api/ping")
+    @GetMapping("/ping")
     public Map<String, String> ping() {
         return Map.of("message", "pong");
+    }
+    @GetMapping("/debug/count")
+    public Map<String, Integer> count() {
+        return Map.of("total", repo.findAll().size());
     }
 }
